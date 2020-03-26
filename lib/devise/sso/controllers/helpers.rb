ef5f6b3ef9951bed95f110880db3f113a225260c
Wawan Kurniawan <ones07@gmail.com>
2020-03-27 00:55:58 +0700

    add default sso_login_url

--- lib/devise/sso/controllers/helpers.rb
+++ lib/devise/sso/controllers/helpers.rb
@@ -9,16 +9,27 @@ module Devise
           redirect_to sso_login_url unless sso_resource_signed_in?
         end
 
-        def sso_login_url
-          '/' # Override this on your controller
-        end
-
         def sso_resource_signed_in?
           current_sso_resource.present?
         end
 
         def current_sso_resource
           @current_sso_resource ||= Devise::Sso::Authentication.find(cookies[Devise::Sso.session_key])
+        end
+
+        def sso_login_url
+          Devise::Sso::UrlBuilder.new(
+            host: ENV.fetch('SSO_HOST'),
+            port: ENV.fetch('SSO_PORT'),
+            path: ENV.fetch('SSO_LOGIN_PATH', 'sso/sessions/login'),
+            query: {
+              sso_redirect: sso_redirect_location
+            }
+          ).to_s
+        end
+
+        def sso_redirect_location
+          session[:sso_redirect] || request.url
         end
       end
     end
