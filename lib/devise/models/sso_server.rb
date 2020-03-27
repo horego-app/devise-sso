9118451b66318182297e50fa6e59fdfdb115e543
Wawan Kurniawan <ones07@gmail.com>
2020-03-27 12:50:52 +0700

    update authentication_token generation

--- lib/devise/models/sso_server.rb
+++ lib/devise/models/sso_server.rb
@@ -4,13 +4,20 @@ module Devise
   module Models
     module SsoServer
       def generate_authentication_token!
-        self.authentication_token = SecureRandom.hex.slice(0, 30)
+        self.authentication_token = generate_authentication_token
         save!
       end
 
       def reset_authentication_token!
         self.authentication_token = nil
         save!
+      end
+
+      def generate_authentication_token
+        loop do
+          token = Devise.friendly_token
+          break token unless self.class.find_by(authentication_token: token)
+        end
       end
     end
   end
